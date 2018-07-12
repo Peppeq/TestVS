425f3370af7869496daa4b2ff34919e753b0d731
Peter Hladky <peto.hladky@gmail.com>
2018-07-12 12:15:17 +0200

    adding controller + view changing angular to partial page

--- SupercoolTest/Startup.cs
+++ SupercoolTest/Startup.cs
@@ -22,13 +22,20 @@ namespace SupercoolTest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
             app.Use(async (context, next) => {
                 await next();
                 if (context.Response.StatusCode == 404 &&
                    !Path.HasExtension(context.Request.Path.Value) &&
                    !context.Request.Path.Value.StartsWith("/api/"))
                 {
-                    context.Request.Path = "/index.html";
+                    context.Request.Path = "/AngularTest.html";
                     await next();
                 }
             });
